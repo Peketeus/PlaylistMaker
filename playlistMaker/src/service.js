5e05effb0af59431ea51ca7dfd09029dd606d42f
Peketeus <p3keteus@gmail.com>
2024-10-08 14:09:24 +0300

    Pientä hiomista ja log haetulle biisille

--- playlistMaker/src/service.js
+++ playlistMaker/src/service.js
@@ -133,12 +133,14 @@ export async function apiCallClick(params) {
   console.log("Api call parametrilla: ", params)
 
   // yhdistää yksittäisen biisin hakuun id:n (params)
-  let endpoint = "https://api.spotify.com/v1/tracks/".concat(params)
+  const endpoint = "https://api.spotify.com/v1/tracks/".concat(params)
 
   const track = await apiCall(endpoint)
+  
+  console.log(track)
 }
 
-// apikutsu
+// apikutsu. Muut funktiot luovat osoitteen, jonka jälkeen tätä kutsutaan
 export async function apiCall(params) {
   const response = await fetch(params, {
     method: 'GET',
