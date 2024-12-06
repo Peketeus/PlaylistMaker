4c5987ce4555af90d93bebc2da82d213859e77d1
ntkfi <ntkumpul@student.jyu.fi>
2024-12-06 14:42:24 +0200

    header tehty pienelle näytölle ystävälliseksi

--- playlistMaker/src/components/HeaderBar.jsx
+++ playlistMaker/src/components/HeaderBar.jsx
@@ -4,27 +4,28 @@ import About from './HeaderLinks/About';
 import TermsOfService from './HeaderLinks/TermsOfService';
 import { logoutClick } from '../service';
 
-function HeaderBar({isLoggedIn}) {
-    return (
-      <div className="flex w-full h-[10vh] mb-8 relative bg-[#383b3b]">
-        <div className='spotify-watermark'>
-            <p>Powered by</p><img src={SpotifyLogo} alt='Spotify' />
-        </div>
-
-        {/* Render log out button only if user is logged in */}
-        {isLoggedIn ? (
-            <div className='absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]'>
-                <button onClick={logoutClick}>Log Out</button>
-            </div>
-        ) : (null)}
-
-        <ul className='w-[85%] flex justify-end items-center gap-16 mr-8'>
-            <li><About /></li>
-            <li><PrivacyPolicy /></li>
-            <li><TermsOfService /></li>
-        </ul>
+function HeaderBar({ isLoggedIn }) {
+  return (
+    <div className="headerbar flex w-full h-[auto] min-h-[50px] mb-8 relative bg-[#383b3b]">
+      <div className='spotify-watermark'>
+        <p>Powered by</p><img src={SpotifyLogo} alt='Spotify' />
       </div>
-    );
-  }
-  
-  export default HeaderBar;
+
+      <ul className='w-[85%] flex justify-end items-center gap-16 mr-8'>
+        <li><About /></li>
+        <li><PrivacyPolicy /></li>
+        <li><TermsOfService /></li>
+      </ul>
+
+      {/* Render log out button only if user is logged in */}
+      {isLoggedIn ? (
+        <div className='logout absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]'>
+          <button onClick={logoutClick}>Log Out</button>
+        </div>
+      ) : (null)}
+
+    </div>
+  );
+}
+
+export default HeaderBar;
